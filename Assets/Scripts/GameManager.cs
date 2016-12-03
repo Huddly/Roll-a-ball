2b3bdf44b262775d282d336a3a07fbff797324c7
Håvard P. Alstad <havard@huddly.com>
2016-12-03 16:33:13 +0100

    added game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance = null;
+
+	// Use this for initialization
+	void Start () {
+        //Check if instance already exists
+        if (instance == null)
+			instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+		DontDestroyOnLoad(gameObject);
+
+		// setup    		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// update
+	}
+}
