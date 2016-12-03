5acfd4b8ecc4f5ed4ca84f0a7a801e748ab8ad3b
Mats <matsglj@matsglj.com>
2016-12-03 23:11:56 +0100

    add timer

--- send_msg.py
+++ send_msg.py
@@ -1,9 +1,9 @@
 import socket
-
+import time
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("127.0.0.1", 8123))
 for i in range(1,100):
     print(i)
     sock.sendall(b'Msg\n')
-
+    time.sleep(0.1)
 sock.close()
